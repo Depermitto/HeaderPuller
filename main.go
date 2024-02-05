df0bbfdc7256f75e07546ed6187b9e6e3a726fb4
permito <penciller94@gmail.com>
2024-02-05 11:50:20 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,15 @@
+﻿package main
+
+import (
+	"github.com/urfave/cli/v2"
+	"log"
+	"os"
+)
+
+func main() {
+	app := cli.App{}
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatalf("Error running hp, %v", err)
+	}
+}
